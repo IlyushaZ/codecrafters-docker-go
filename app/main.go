510d07411085c52dac4111c23cc05a003593d089
Ilya Zyabirov <zyabirov@yandex.ru>
2022-06-21 22:37:19 +0300

    pass 2nd stage [2]

--- app/main.go
+++ app/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
@@ -12,10 +11,10 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
 		os.Exit(1)
 	}
-
-	fmt.Println(string(output))
 }
